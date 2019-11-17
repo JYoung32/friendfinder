f8f9daa7169ce3a7db901ac4f522c9004b2112d8
Joseph Young <joseph.young32@yahoo.com>
2019-11-17 17:19:51 -0500

    html routing page created

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //Dependencies
-var express = require(`express`);
-var path = require(`path`);
+var express = require('express');
+var path = require('path');
 
 //Setting up Express
 var app = express();
@@ -11,8 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Router
-require(`./app/routing/apiRoutes`)(app);
-require(`./app/routing/htmlRoutes.js`)(app);
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT, function() {
     console.log(`App listening on PORT: ${PORT}`);
