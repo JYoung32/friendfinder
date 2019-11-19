995af16d85d0643b4220f5b1cce74ff742ed06f4
Joseph Young <joseph.young32@yahoo.com>
2019-11-19 17:10:16 -0500

    added mock data to friends data page, created comparison logic on the api post route to find closest match

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,16 @@
 //Data file
 //create an array to store data on friends that were input
 
-var friends = [];
+var friends = [
+    {
+        name: "Joe Young",
+        photo: "Example",
+        scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5",]
+    },{
+        name: "Rocco",
+        photo: "Example2",
+        scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+    }
+];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,13 +12,40 @@ module.exports = function(app) {
     //api POST request
     //compatability logic goes here
     app.post("/api/friends", function(req, res) {
+        
+        var newFriend = req.body;
+        //loop through score to make an array of num instead of strings
+        for (var i = 0; i < newFriend.scores.length; i++) {
+            newFriend.scores[i] = parseInt(newFriend.scores[i]);
+        };
+        
+        //default match to first friend but will change after function loops through to find minimum difference
+        var newBestieIndex = 0;
+        //max score is 50, minimum is 10, set difference to max and compare after loop
+        var scoreDifference = 40;
+
+        //loop through the friends data
+        for (var i = 0; i < friends.length; i++) {
+            var friendScoreDifference = 0;
+            //loop through individual score
+            for (var j = 0; j < friends[i].scores.length; j++) {
+                var comparedScoreDifference = Math.abs(newFriend.scores[j] - friends[i].scores[j]);
+                friendScoreDifference += comparedScoreDifference;
+            }
+
+            //if there is a new low score difference, change bestie index
+            if (friendScoreDifference < scoreDifference) {
+                newBestieIndex = i;
+                scoreDifference = friendScoreDifference;
+            }
+        };
+
         //post user to friends array
         friends.push(req.body);
 
-        var newFriend = req.body;
-        console.log(newFriend);
-
-
-        res.json(true);
+        //send bestie with lowest difference to the page
+        res.json(friends[newBestieIndex]);
+        console.log(friends[newBestieIndex]);
+        console.log(scoreDifference);
     });
 };
