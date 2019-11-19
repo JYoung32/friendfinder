f66852d183a7dc2d1750ebd06f8062843a8ff5bc
Joseph Young <joseph.young32@yahoo.com>
2019-11-19 15:52:43 -0500

    created form validation logic for survey html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,8 +10,15 @@ module.exports = function(app) {
     });
 
     //api POST request
+    //compatability logic goes here
     app.post("/api/friends", function(req, res) {
+        //post user to friends array
         friends.push(req.body);
+
+        var newFriend = req.body;
+        console.log(newFriend);
+
+
         res.json(true);
     });
 };
